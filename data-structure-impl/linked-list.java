f5cc3f9775f4561953437907e30ef4f1c2b38784
ricky-kiva <ricky.kiva@gmail.com>
2025-04-29 14:02:06 +0700

    feat(ds-impl): add Linked List impl.

---
+++ data-structure-impl/linked-list.java
@@ -0,0 +1,19 @@
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = 0;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+    Node tail;
+
+    public LinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+}
