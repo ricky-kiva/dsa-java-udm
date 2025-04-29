544e63666bae0491655d291a20380a455d123112
ricky-kiva <ricky.kiva@gmail.com>
2025-04-29 14:33:30 +0700

    feat(ds-impl): add Linked List `printList` method impl.

--- data-structure-impl/linked-list.java
+++ data-structure-impl/linked-list.java
@@ -43,4 +43,15 @@ class LinkedList {
         newNode.next = head;
         head = newNode;
     }
+
+    public void printList() {
+        Node current = head;
+
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+
+        System.out.println();
+    }
 }
